27b16a5596ef3a84b36cb92339f98cc2e1cb1205
Roh Sang Cheol <nscoo@naver.com>
2024-05-06 14:37:37 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,9 +1,3 @@
 def solution(seoul):
-    answer = ''
-    index = 0
-    for _ in seoul:
-        if _ == "Kim":
-            break
-        index+=1
-    answer = "김서방은 " + str(index)+"에 있다"
-    return answer
+    
+    return "김서방은 " + str(seoul.index("Kim")) + "에 있다"
