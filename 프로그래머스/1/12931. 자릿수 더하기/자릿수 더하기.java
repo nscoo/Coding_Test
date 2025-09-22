7f54e5f8479085aacd7d6b56a8caba355cb6c6b4
Roh Sang Cheol <nscoo@naver.com>
2025-09-22 21:05:55 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(n<10) return n;
+        while(n/10 !=0){
+            answer += n%10;
+            n/=10;
+        }
+        answer+=n;
+        
+        return answer;
+    }
+}
