84d98b90cc2324fe993de69477d961c8a8bcdfa1
Roh Sang Cheol <nscoo@naver.com>
2025-10-06 11:45:33 +0900

    [level 2] Title: 주식가격, Time: 16.17 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,13 +4,10 @@ class Solution {
         for(int i=0; i<prices.length-1; i++){
             int cnt = 0;
             for(int j=i+1; j<prices.length; j++){
-                if(prices[i]<=prices[j]) cnt++;
-                else{
-                    answer[i] = ++cnt;
-                    break;
-                }
-                answer[i] = cnt;
+                cnt++;
+                if(prices[i]>prices[j]) break;
             }
+            answer[i] = cnt;
         }
         answer[prices.length-1] = 0;
         return answer;
